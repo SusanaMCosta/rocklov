57d0d34f2ce62071d189a025438f3bd9b704d9c1
SusanaMCosta <suzanasuzy12@hotmail.com>
2021-05-26 14:34:07 -0300

    Conclusão dos testes de cadastro

--- features/cadastro.feature
+++ features/cadastro.feature
@@ -5,31 +5,35 @@ Funcionalidade: Cadastro
     Quero fazer o meu cadastro no RockLov
     Para que eu possa disponibilizá-los para locação
 
-    @temp
+    @macgyver
     Cenario: Fazer cadastro
 
         Dado que acesso a página de cadastro
         Quando submeto o meu cadastro completo
         Então sou redirecionado para o Dashboard
 
+    @sem_nome
     Cenario: Submeter cadastro sem o nome
 
         Dado que acesso a página de cadastro
         Quando submeto o meu cadastro sem o nome
         Então vejo a mensagem de alerta: Oops. Informe seu nome completo!
 
+    @sem_email
     Cenario: Submeter cadastro sem o email
 
         Dado que acesso a página de cadastro
         Quando submeto o meu cadastro sem o email
         Então vejo a mensagem de alerta: Oops. Informe um email válido!
 
+    @email_incorreto
     Cenario: Submeter cadastro com email incorreto
 
         Dado que acesso a página de cadastro
-        Quando submeto o meu cadastro com incorreto
+        Quando submeto o meu cadastro com email incorreto
         Então vejo a mensagem de alerta: Oops. Informe um email válido!
 
+    @sem_senha
     Cenario: Submeter cadastro sem a senha
 
         Dado que acesso a página de cadastro
--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -1,13 +1,68 @@
+
 Dado('que acesso a página de cadastro') do
     visit "http://rocklov-web:3000/signup"
-    sleep 10 # temporario
 end
   
 Quando('submeto o meu cadastro completo') do
-    
+    find("#fullName").set "Susana Moreira"
+    find("#email").set Faker::Internet.free_email
+    find("#password").set "123"
+
+    click_button "Cadastrar"
 end
   
 Então('sou redirecionado para o Dashboard') do
-    pending # Write code here that turns the phrase above into concrete actions
+    expect(page).to have_css ".dashboard"
 end
+  
+
+Quando('submeto o meu cadastro sem o nome') do
+    find("#email").set Faker::Internet.free_email
+    find("#password").set "123"
+
+    click_button "Cadastrar"
+end
+  
+Então('vejo a mensagem de alerta: Oops. Informe seu nome completo!') do
+    alert = find(".alert-dark")
+    expect(alert.text).to eql "Oops. Informe seu nome completo!"
+end
+
+
+Quando('submeto o meu cadastro sem o email') do
+    find("#fullName").set "Susana Moreira"
+    find("#password").set "123"
+
+    click_button "Cadastrar"
+end
+  
+Então('vejo a mensagem de alerta: Oops. Informe um email válido!') do
+    alert = find(".alert-dark")
+    expect(alert.text).to eql "Oops. Informe um email válido!"
+    sleep 10
+end
+
+Quando('submeto o meu cadastro com email incorreto') do
+    find("#fullName").set "Susana Moreira"
+    find("#email").set "su@su"
+    find("#password").set "123"
+
+    click_button "Cadastrar"
+end
+
+Quando('submeto o meu cadastro sem a senha') do
+    find("#fullName").set "Susana Moreira"
+    find("#email").set Faker::Internet.free_email
+
+    click_button "Cadastrar"
+end
+  
+Então('vejo a mensagem de alerta: Oops. Informe sua senha secreta!') do
+    alert = find(".alert-dark")
+    expect(alert.text).to eql "Oops. Informe sua senha secreta!"
+    sleep 10
+end
+  
+  
+  
   
