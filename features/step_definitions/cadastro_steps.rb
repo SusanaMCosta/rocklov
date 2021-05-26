7c048896674af1c0619278d8571e0acb51b7a604
Susana Maria Costa Moreira <suzanasuzy12@hotmail.com>
2021-05-26 10:00:50 -0300

    Inicio do projeto

---
+++ features/step_definitions/cadastro_steps.rb
@@ -0,0 +1,13 @@
+Dado('que acesso a página de cadastro') do
+    visit "http://rocklov-web:3000/signup"
+    sleep 10 # temporario
+end
+  
+Quando('submeto o meu cadastro completo') do
+    
+end
+  
+Então('sou redirecionado para o Dashboard') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
